86b298574c49b5782672c141fc80e43649caab6b
BrianLecarpentier <brian.lecarpentier@teolia.fr>
2022-10-26 14:55:47 +0200

    Add socle task

---
+++ task_socle/task_socle.ino
@@ -0,0 +1,210 @@
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <ArduinoJson.h>
+
+#include <WebSocketsClient.h>
+#include <SocketIOclient.h>
+
+#define USE_SERIAL Serial
+
+WiFiMulti WiFiMulti;
+SocketIOclient socketIO;
+
+const int button1 = 13;
+const int button2 = 15;
+const int button3 = 2;
+const int led1 = 12;
+const int led2 = 14;
+const int led3 = 27;
+
+bool taskEnabled = true;
+
+const char* ssid_board = "SOCLE";
+const char* password_board = "12345678";
+const char* ssid = "SFR_45EF";
+const char* password = "d9byza2yhvc92dfebfi7";
+const char* host = "192.168.1.149";
+const int port = 3000;
+const char* path = "/socket.io/?EIO=4";
+
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
+    switch(type) {
+        case sIOtype_DISCONNECT:
+            USE_SERIAL.printf("[IOc] Disconnected!\n");
+            break;
+        case sIOtype_CONNECT:
+            USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
+
+            // join default namespace (no auto join in Socket.IO V3)
+            socketIO.send(sIOtype_CONNECT, "/");
+            break;
+        case sIOtype_EVENT:
+        {
+            char * sptr = NULL;
+            int id = strtol((char *)payload, &sptr, 10);
+            USE_SERIAL.printf("[IOc] get event: %s id: %d\n", payload, id);
+            if(id) {
+              payload = (uint8_t *)sptr;
+            }
+            DynamicJsonDocument doc(1024);           
+            DeserializationError error = deserializeJson(doc, payload, length);
+            if(error) {
+                USE_SERIAL.print(F("deserializeJson() failed: "));
+                USE_SERIAL.println(error.c_str());
+                return;
+            }
+
+
+            String eventName = doc[0];
+            if (eventName == "enableTaskSocle") {
+                taskEnabled = true;
+                USE_SERIAL.printf("task enabled\n");
+            } else if (eventName == "disableTaskSocle") {
+                taskEnabled = false;
+                initTask();
+                USE_SERIAL.printf("task disabled\n");
+            } else if (eventName == "taskCompletedSocle") {
+                taskEnabled = false;
+                USE_SERIAL.printf("task disabled\n");
+            } else if (eventName == "taskLedSocle") {
+              String led = doc[1]["led"];
+              taskLed(led);
+            }
+         } 
+            break;
+        case sIOtype_ACK:
+            USE_SERIAL.printf("[IOc] get ack: %u\n", length);
+            break;
+        case sIOtype_ERROR:
+            USE_SERIAL.printf("[IOc] get error: %u\n", length);
+            break;
+        case sIOtype_BINARY_EVENT:
+            USE_SERIAL.printf("[IOc] get binary: %u\n", length);
+            break;
+        case sIOtype_BINARY_ACK:
+            USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
+            break;
+    }
+}
+
+void setup() {
+    setupPin();
+    initTask();
+    USE_SERIAL.begin(115200);
+    USE_SERIAL.setDebugOutput(true);
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid_board, password_board);
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP(ssid, password);
+
+    //WiFi.disconnect();
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    String ip = WiFi.localIP().toString();
+    USE_SERIAL.printf("[SETUP] WiFi Connected %s\n", ip.c_str());
+
+    // server address, port and URL
+    socketIO.begin(host, port, path);
+
+    // event handler
+    socketIO.onEvent(socketIOEvent);
+}
+
+unsigned long messageTimestamp = 0;
+void loop() {
+  socketIO.loop();
+  taskSocle();
+  delay(500);
+}
+
+void setupPin() {
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void initTask() {
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+}
+
+void taskSocle() {
+  if(taskEnabled) {
+    // creat JSON message for Socket.IO (event)
+    DynamicJsonDocument doc(1024);
+    JsonArray array = doc.to<JsonArray>();
+
+    // add evnet name
+    // Hint: socket.on('event_name', ....
+    array.add("taskSocle");
+
+    // add payload (parameters) for the event
+    JsonObject param1 = array.createNestedObject();
+    
+    if (digitalRead(button1) == LOW) {
+        param1["button1"] = true;
+        digitalWrite(led1, HIGH);
+    } else {
+      digitalWrite(led1, LOW);
+    }
+    if (digitalRead(button2) == LOW) {
+        param1["button2"] = true;
+        digitalWrite(led2, HIGH);
+    } else {
+        digitalWrite(led2, LOW);
+    }
+    if (digitalRead(button3) == LOW) {
+        param1["button3"] = true;
+        digitalWrite(led3, HIGH);
+    } else {
+        digitalWrite(led3, LOW);
+    }
+    
+    // JSON to String (serializion)
+    String output;
+    serializeJson(doc, output);
+
+    // Send event
+    socketIO.sendEVENT(output);
+
+    // Print JSON for debugging
+    USE_SERIAL.println(output);
+  }
+}
+
+void taskLed(String led) {
+  if (led == "led1") {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  } else if (led == "led2") {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+  } else if (led == "led3") {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+  } else {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  }
+}
